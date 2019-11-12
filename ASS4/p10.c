77c78dbd3766ec82b69c5d5bd935d5cee96556ff
veedee <43088920+veedee2000@users.noreply.github.com>
2019-11-12 21:27:01 +0530

    BST for Searching

--- ASS4/p10.c
+++ ASS4/p10.c
@@ -1,35 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int c;
+
 struct tree{
-    int val;
+    int data;
     struct tree* left;
     struct tree* right;
-}
+};
 
-struct tree* create(){
-    struct tree* current = q.front();
-    q.pop();
-    current -> 
-}
-
-struct tree* getnode(int val){
+struct tree* create(int val){
     struct tree* node = (struct tree*)(malloc(sizeof(struct tree)));
-    node -> val = val;
+    node -> data = val;
     node -> left = NULL;
     node -> right = NULL;
+    return node;
+}
+
+struct tree* insert(struct tree* node, int val){
+    if(!node) return create(val);
+    else if(node -> data >= val) node -> left = insert(node -> left,val);
+    else node -> right = insert(node -> right,val);
+    return node;
+}
+
+void search(struct tree* node, int val){
+    if(!node) {c = 0; return;}
+    if(node -> data == val) {c = 1; return;}
+    else if(node -> data < val) search(node -> right,val);
+    else search(node -> left, val);
 }
 
 int main(){
+
     int n;
-    printf("Enter the number of nodes to be inserted: ");
+    printf("Enter the size: ");
     scanf("%d",&n);
-    int i;
-    struct tree* root = 
-    for(i = 0;i < n;i+=2){
-
+    struct tree* root = NULL;
+    while(n--){
+        int val;
+        printf("Enter the value: ");
+        scanf("%d",&val);
+        root = insert(root,val);
     }
-
+    printf("Enter the value to be searced: ");
+    int val;
+    scanf("%d",&val);
+    search(root, val);
+    if(c) printf("Element found\n");
+    else printf("Element not found\n");
     return 0;
 }
-
